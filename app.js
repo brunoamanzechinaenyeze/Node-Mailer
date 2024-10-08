50933fb24ea836d704b0675ec5ab8a5059b3474e
Amanze Bruno <heroicemblem69@gmail.com>
2024-10-08 11:35:20 +0100

    Adding functionalities to the nodemailer

--- app.js
+++ app.js
@@ -7,9 +7,25 @@ const html = `
 
 async function main () {
 
-    nodeMailer.createTransport({
-        
+   const transporter = nodeMailer.createTransport({
+         host: 'mail.openjavascript.info',
+         port: 465,
+         secure: true,
+         auth: {
+            user: 'test@openjavascript.info',
+            pass: 'NodeMailer123'
+         }
     })
+
+    const info = await transporter.sendMail({
+        from: 'OpenJavascript <test@openjavascript.info>',
+        to: 'heroicemeblem69@gmail.com',
+        subject: 'Test Email',
+        html: html
+    })
+
+    console.log('Message sent: ' + info.messageId);
 }
 
-main();
+main()
+.catch(e => console.log(e))
