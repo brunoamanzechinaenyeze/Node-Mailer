d6611e3330e01db8c1247d0d1fe4aa7562dd0c86
Amanze Bruno <heroicemblem69@gmail.com>
2024-10-08 11:25:35 +0100

    Creating the node mailer function

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const nodeMailer = require('nodemailer')
+const nodeMailer = require("nodemailer");
+
+const html = `
+    <h1>Welcome Amanze</h1>
+    <p>You are welcome to our company</p>
+`;
+
+async function main () {
+
+    nodeMailer.createTransport({
+        
+    })
+}
+
+main();
